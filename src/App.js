9d84d2a08c5551367a559e0c330bc63bd77d1f9b
LuisGtk <luisgonzalez189@gmail.com>
2022-07-06 15:28:43 -0400

    installed qrcode dependencies

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import { QRCode } from 'qrcode';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          hello world
+        </p>
+        {/* <a
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
