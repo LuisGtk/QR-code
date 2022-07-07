21738cdcf07dd9b26254b2b5ebcb677acb9bdce2
LuisGtk <luisgonzalez189@gmail.com>
2022-07-07 15:06:14 -0400

    created value for qrcode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import { QRCode } from 'qrcode';
+import { useState, useEffect } from 'react';
+import QRCode from 'qrcode';
 import './App.css';
 
-function App() {
+function App({ text }) {
+  const [src, setSrc] = useState("")
+  useEffect(() => {
+    QRCode.toDataURL(text).then((data) => {
+      setSrc(data)
+    })
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img className='scan' 
-        src={src}/>
+        <img
+          src={src}
+          alt="scannerImg"
+        />
       </header>
     </div>
   );
